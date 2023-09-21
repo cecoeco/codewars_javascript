44e93470c3fc16bc75f255424f89500520ce3878
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-09-21 17:02:22 -0400

    Create Training JS #3: Basic data types--String.js

---
+++ 8 kyu/Training JS #3: Basic data types--String.js
@@ -0,0 +1,26 @@
+var a1 = "A", a2 = "a", b1 = "B", b2 = "b", c1 = "C", c2 = "c", d1 = "D", d2 = "d", e1 = "E", e2 = "e", n1 = "N", n2 = "n";
+
+function Dad() {
+  return d1 + a2 + d2; // Combining d1, a2, and d2 to form "Dad"
+}
+
+function Bee() {
+  return b1 + e2 + e2; // Combining b1, e2, and e2 to form "Bee"
+}
+
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2; // Combining b2, a2, n2, a2, n2, and a2 to form "banana"
+}
+
+// Answering questions
+function answer1() {
+  return "yes";
+}
+
+function answer2() {
+  return "no";
+}
+
+function answer3() {
+  return "yes";
+}
